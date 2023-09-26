0768d51bff538396782807719b32feeaf5e65223
Kenet Picado <kenetpicado1@gmail.com>
2023-09-26 09:43:17 -0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,12 +62,12 @@ class User extends Authenticatable
     public function getOnlineAttribute(): string
     {
         if (!$this->last_login)
-            return 'Not logged in';
+            return 'No ha iniciado sesión';
 
         if ($this->last_login->diffInMinutes() < 4)
-            return 'Now';
+            return 'Activo ahora';
 
-        return $this->last_login->diffForHumans();
+        return "Activo " . $this->last_login->diffForHumans();
     }
 
     public function sellers()
